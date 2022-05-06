91ff09d557da2a8d764d00b4ab3bd569ee2e999b
Cheol-Cheol <dream174822@gmail.com>
2022-05-06 19:56:37 +0900

    ✏️ AppLoading 기능을 SplashScreen 기능으로 구현

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import React, { useState, useEffect, useCallback } from "react";
+import { View, Text } from "react-native";
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    // 데이터를 가져오는 구간
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        // fetch data...
+
+        // 테스트를 위한 타이머 설정
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+      } catch (e) {
+        console.error(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+    <View onLayout={onLayoutRootView}>
+      <Text>We are done loading!</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
