c89e1da6349d9b49a932dab65eeec124496cd07d
Cheol-Cheol <dream174822@gmail.com>
2022-05-09 14:34:19 +0900

    ✏️ styleSheet & styled-components 적용

--- screens/Movies.js
+++ screens/Movies.js
@@ -1,13 +1,21 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
+
+const Btn = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.Text`
+  color: blue;
+`;
 
 const Movies = ({ navigation: { navigate } }) => (
-  <TouchableOpacity
-    onPress={() => navigate("Stack", { screen: "Three" })}
-    style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-  >
-    <Text>Movie</Text>
-  </TouchableOpacity>
+  <Btn onPress={() => navigate("Stack", { screen: "Three" })}>
+    <Title>Movie</Title>
+  </Btn>
 );
 
 export default Movies;
