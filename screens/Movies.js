5eb65875bd5a6f047781a1144a7482a6aab23d62
Cheol-Cheol <dream174822@gmail.com>
2022-05-10 12:16:24 +0900

    ✏️ Movies Screen part

--- screens/Movies.js
+++ screens/Movies.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Dimensions, ActivityIndicator } from "react-native";
 import styled from "styled-components/native";
-import Swiper from "react-native-web-swiper";
+import Swiper from "react-native-swiper";
 import { makeImgPath } from "../utils";
 // import { BlurView } from "expo-blur";
 
@@ -22,7 +22,30 @@ const BgImg = styled.Image`
   position: absolute;
 `;
 const Title = styled.Text`
-  color: #eee;
+  font-weight: 600;
+  color: white;
+`;
+const Poster = styled.Image`
+  width: 100px;
+  height: 155px;
+  border-radius: 5px;
+`;
+const Wrapper = styled.View`
+  flex-direction: row;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+const Column = styled.View`
+  width: 40%;
+  margin-left: 15px;
+`;
+const Overview = styled.Text`
+  margin-top: 10px;
+  color: rgba(255, 255, 255, 0.8);
+`;
+const Votes = styled(Overview)`
+  font-size: 12px;
 `;
 
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
@@ -52,15 +75,26 @@ const Movies = () => {
   ) : (
     <Container>
       <Swiper
+        horizontal
         loop
-        timeout={3.45}
-        controlsEnabled={false}
+        autoplayTimeout={3.45}
+        showsButtons={false}
+        showsPagination={false}
         containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 5 }}
       >
         {nowPlaying.map((movie) => (
           <View key={movie.id}>
             <BgImg source={{ url: makeImgPath(movie.backdrop_path) }} />
-            <Title>{movie.original_title}</Title>
+            <Wrapper>
+              <Poster source={{ url: makeImgPath(movie.poster_path) }}></Poster>
+              <Column>
+                <Title>{movie.original_title}</Title>
+                {movie.vote_average > 0 ? (
+                  <Votes>⭐️{movie.vote_average}/10</Votes>
+                ) : null}
+                <Overview>{movie.overview.slice(0, 90)}...</Overview>
+              </Column>
+            </Wrapper>
           </View>
         ))}
       </Swiper>
