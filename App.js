d67eee4adfa0a462a75867f50d07cb3b6a3c6938
Cheol-Cheol <dream174822@gmail.com>
2022-07-02 20:09:51 +0900

    ✏️ 코드 정리 및 파일 삭제

--- App.js
+++ App.js
@@ -3,47 +3,12 @@ import React, { useState, useCallback } from "react";
 import { useColorScheme } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { ThemeProvider } from "styled-components/native";
-// Expo
-import * as SplashScreen from "expo-splash-screen";
-import { useAssets } from "expo-asset";
-import { Ionicons } from "@expo/vector-icons";
-import * as Font from "expo-font";
 
 import Root from "./navigation/Root";
 import { darkTheme, lightTheme } from "./styled";
 
 export default function App() {
-  const [appIsReady, setAppIsReady] = useState(false);
-  const [assets] = useAssets([require("./image01.jpeg")]);
-  const [loaded] = Font.useFonts(Ionicons.font);
   const isDark = useColorScheme() === "dark";
-  // useEffect(() => {
-  //   // 스플래쉬 스크린 중 데이터를 가져오는 구간
-  //   async function prepare() {
-  //     try {
-  //       if (!assets || !loaded) {
-  //         await SplashScreen.preventAutoHideAsync();
-  //       }
-  //       // fetch data...
-  //     } catch (e) {
-  //       console.error(e);
-  //     } finally {
-  //       setAppIsReady(true);
-  //     }
-  //   }
-  //   prepare();
-  // }, []);
-
-  const onLayoutRootView = useCallback(async () => {
-    // 재렌더링이 되더라도 appIsReady(현재 true)값이 바뀌지 않는 이상, hideAsync() 함수를 계속 재사용 가능하다.
-    if (appIsReady) {
-      await SplashScreen.hideAsync();
-    }
-  }, [appIsReady]);
-
-  // if (!appIsReady) {
-  //   return null;
-  // }
 
   return (
     <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
